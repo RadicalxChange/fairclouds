5963b5c73bb85440fc00098ad65a433b0f655cd4
Jack MB <jack@designandcode.com>
2024-06-05 21:26:42 +0200

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "en",
-    locales: ["es", "en"],
-  }
+    locales: ["es", "en"]
+  },
+  adapter: cloudflare()
 });
