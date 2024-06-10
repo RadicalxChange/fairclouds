bfe739a84bd66ed5240111bed8c72cf5ae75d0e7
Jack MB <jack@designandcode.com>
2024-06-10 14:49:30 +0200

    adjust astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,18 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  // site: 'https://fairclouds.life',
+  site: 'https://fairclouds.life',
   integrations: [tailwind()],
   // i18n: {
   //   defaultLocale: "en",
   //   locales: ["es", "en"]
   // },
   adapter: cloudflare({}),
+  vite: {
+    define: {
+      'process.env.PUBLIC_STRIPE_KEY': JSON.stringify(process.env.PUBLIC_STRIPE_KEY),
+      'process.env.STRIPE_KEY': JSON.stringify(process.env.STRIPE_KEY),
+      'process.env.WEATHER_KEY': JSON.stringify(process.env.WEATHER_KEY)
+    }
+  }
 });
