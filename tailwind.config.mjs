a504852427ea0672817ec4636eed4d4a6c55bd05
Jack MB <jack@designandcode.com>
2024-06-24 16:11:07 +0200

    implement landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,13 @@ export default {
 	theme: {
 		extend: {
 			colors: {
+				primary: '#72AEE9',
+				white: '#FFFFFF',
 				grey: '#D9D9D9'
 			},
+			boxShadow: {
+        'cloud': 'inset 0px 0px 6.8px 3px #FFFFFF',
+      }
 		},
 	},
 	plugins: [],
