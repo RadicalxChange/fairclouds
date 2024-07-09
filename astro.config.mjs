a849c3e39a4c7d1c85b329c6462eda5a43f7c20e
Jack MB <jack@designandcode.com>
2024-07-09 15:31:27 +0200

    code and style menu component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+import svelte from '@astrojs/svelte';
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://fairclouds.life',
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
